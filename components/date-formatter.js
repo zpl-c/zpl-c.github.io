0c118627c31a93431f78343eac0cc6e800ed640b
zaklaus <zaklaus@madaraszd.net>
2022-06-09 16:26:50 +0000

    Deploying to gh-pages from @ zpl-c/zpl-c.github.io@8ceaa1afe999c00847f73e8ad4800dc5d59d1580 🚀

--- components/date-formatter.js
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export default function DateFormatter({ dateString, className }) {
-  const date = parseISO(dateString)
-  return <time className="text-gray-500" dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
-}
