8316de9320e5e2d51daafaa5131915633e809ea2
Dominik Madarász <zaklaus@outlook.com>
2022-06-10 13:15:07 +0000

    update footer

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://zpl.pw/fetch"
+              href="https://github.com/zpl-c/zpl/releases/latest/download/zpl.h"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Download latest release
